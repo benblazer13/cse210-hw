6ba4dcb6c692769d804dc49d79eae8f4ac7879df
benblazer13 <benblazer2020@gmail.com>
2026-02-17 18:07:18 -0500

    final project complete

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    protected int GetMinutes() => _minutes;
+
+    protected string GetDateString() => _date.ToString("dd MMM yyyy");
+
+    public virtual double GetDistance() => 0;
+    public virtual double GetSpeed() => 0;
+    public virtual double GetPace() => 0;
+
+    public virtual string GetSummary()
+    {
+        return $"{GetDateString()} {GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():0.0} miles, " +
+               $"Speed: {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
+}
